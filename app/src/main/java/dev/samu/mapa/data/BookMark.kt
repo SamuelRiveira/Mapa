6794460ae87dec667f91db8cd1d0a31494a83f47
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-12-04 08:47:34 +0000

    Initial commit

---
+++ app/src/main/java/dev/samu/mapa/data/BookMark.kt
@@ -0,0 +1,12 @@
+package dev.samu.mapa.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bookmark_table")
+data class Bookmark(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val url: String,
+    val typeId: Int
+)
